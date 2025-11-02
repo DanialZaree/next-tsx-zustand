345360bbc1f41026d05a52a91c750222336c35d8
Danial Zaree <danialzaree0@gmail.com>
2025-11-03 00:25:33 +0330

    some changes

--- store/store.ts
+++ store/store.ts
@@ -1,28 +1,25 @@
 import { createStore } from "zustand/vanilla";
 
-export type StoreState = {
-    stateValue: number;
-}
+export type Store = {
+  stateValue: number;
+  increment: () => void;
+  decrement: () => void;
+  reset: () => void;
+};
 
-export type StoreActions = {
-    increment: () => void;
-    decrement: () => void;
-    reset: () => void;
-}
-
-export type store  = StoreState & StoreActions;
-
-export const defaultState: StoreState = {
-    stateValue: 0,
+export const defaultState = {
+  stateValue: 0,
 };
 
 export const createStoreMe = (
-  initState: StoreState = defaultState,
+  initState = defaultState, 
 ) => {
-  return createStore<StoreState>()((set) => ({
+  return createStore<Store>()((set) => ({
     ...initState,
     increment: () => set((state) => ({ stateValue: state.stateValue + 1 })),
     decrement: () => set((state) => ({ stateValue: state.stateValue - 1 })),
-    reset: () => set({ stateValue: 0 }),
-  }))
-}
+    reset: () => set({ stateValue: 0 }), 
+  }));
+};
+
+export const myStore = createStoreMe();
